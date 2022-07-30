8dd45bc7a4da80c68dd1e6ec94f05e2236c93c43
Furkan Deniz <76913129+asabibey@users.noreply.github.com>
2022-07-31 02:39:07 +0300

    Add files via upload

---
+++ CountDıstınct.sql
@@ -0,0 +1,21 @@
+-- 1.soru
+-- SELECT DISTINCT replacement_cost FROM film
+
+-- 2.soru
+-- SELECT COUNT (DISTINCT replacement_cost) FROM film 
+-- 21 farklı veri vardır.
+
+-- 3.soru
+-- SELECT COUNT (*) FROM film
+-- WHERE title LIKE 'T%' AND rating = 'G' ;
+-- 9 veri vardır.
+
+-- 4.soru
+-- SELECT COUNT (country) FROM country
+-- WHERE country LIKE ('_____')
+-- 13 veri vardır.
+
+-- 5.soru 
+-- SELECT COUNT (city) FROM city
+-- WHERE city LIKE ('R%r')
+-- 1 veri vardır 
